047ecc75508c1b027342bcb6329820cad6c6fefc
Secret Company <secretcompany70@gmail.com>
2024-08-08 08:27:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -89,17 +89,17 @@ logo = r + f'''
 ascii = pystyle.Center.XCenter(logo)
 def menu():
     options = f''' 
-    ╔═══                              ═══╗ ╔═══                               ═══╗ ╔═══                                 ═══╗
-    ║   ({b}01{r}) > C2DDOS              ║ ║         ({b}10{r}) > N/A            ║ ║               ({b}19{r}) > N/A        ║
-        ({b}02{r}) > C7DDOS                          ({b}11{r}) > N/A                              ({b}20{r}) > N/A
-        ({b}03{r}) > DoxTracking                     ({b}12{r}) > N/A                              ({b}21{r}) > N/A
-        ({b}04{r}) > PhoneLookup                     ({b}13{r}) > N/A                              ({b}22{r}) > N/A
-        ({b}05{r}) > UsernameChecker                 ({b}14{r}) > N/A                              ({b}23{r}) > N/A
-        ({b}06{r}) > N/A                             ({b}15{r}) > N/A                              ({b}24{r}) > N/A
-        ({b}07{r}) > N/A                             ({b}16{r}) > N/A                              ({b}25{r}) > N/A
-        ({b}08{r}) > N/A                             ({b}17{r}) > N/A                              ({b}26{r}) > N/A
-    ║   ({b}09{r}) > N/A                 ║ ║         ({b}18{r}) > N/A             ║ ║              ({b}27{r}) > N/A        ║
-    ╚═══                              ═══╝ ╚═══                                ═══╝ ╚═══                                ═══╝
+    ╔═══                              ═══╗ ╔═══                         ═══╗ ╔═══                      ═══╗
+    ║   ({b}01{r}) > C2DDOS              ║ ║   ({b}10{r}) > N/A            ║ ║   ({b}19{r}) > N/A         ║
+        ({b}02{r}) > C7DDOS                    ({b}11{r}) > N/A                  ({b}20{r}) > N/A
+        ({b}03{r}) > DoxTracking               ({b}12{r}) > N/A                  ({b}21{r}) > N/A
+        ({b}04{r}) > PhoneLookup               ({b}13{r}) > N/A                  ({b}22{r}) > N/A
+        ({b}05{r}) > UsernameChecker           ({b}14{r}) > N/A                  ({b}23{r}) > N/A
+        ({b}06{r}) > N/A                       ({b}15{r}) > N/A                  ({b}24{r}) > N/A
+        ({b}07{r}) > N/A                       ({b}16{r}) > N/A                  ({b}25{r}) > N/A
+        ({b}08{r}) > N/A                       ({b}17{r}) > N/A                  ({b}26{r}) > N/A
+    ║   ({b}09{r}) > N/A                 ║ ║   ({b}18{r}) > N/A             ║ ║  ({b}27{r}) > N/A         ║
+    ╚═══                              ═══╝ ╚═══                          ═══╝ ╚═══                     ═══╝
 ''' 
     os.system('cls' if os.name == 'nt' else 'clear')
     ops = pystyle.Center.XCenter(options)
